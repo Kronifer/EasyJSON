30643a14e1467186b8a5e7ea9675455a755bbdae
Kronifer <44979306+Kronifer@users.noreply.github.com>
2021-02-23 11:00:18 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from distutils.core import setup
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 setup(
   name = 'kvJSON',         # How you named your package folder (MyLib)
   packages = ['kvJSON'],   # Chose the same as "name"
-  version = '1.4.0',      # Start with a small number and increase it with every change you make
+  version = '1.4.1',
+  description = 'An easy way to work with JSON in Python.',
+  long_description=long_description,
+  long_description_content_type='text/markdown',# Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   author = 'Dillon Runke',                   # Type in your name
   author_email = 'dillonr5@live.wsd1.org',      # Type in your E-Mail
   url = 'https://github.com/Kronifer/kvJSON',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Kronifer/kvJSON/archive/v1.4.2.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Kronifer/kvJSON/archive/v1.4.3.tar.gz',    # I explain this later on
   keywords = ['JSON', 'Data Storage'],   # Keywords that define your package best
   classifiers=[
     'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
