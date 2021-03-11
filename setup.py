ad824a406d910fe730188e7ff39fe66c352150cc
Dillon Runke <44979306+Kronifer@users.noreply.github.com>
2021-03-11 09:43:46 -0600

    changed to setuptools

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
-from distutils.core import setup
+from setuptools import setup
 
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_des = f.read()
 setup(
   name = 'kvJSON',         # How you named your package folder (MyLib)
   packages = ['kvJSON'],   # Chose the same as "name"
